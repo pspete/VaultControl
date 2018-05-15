6a91a8205b319cc0a4a2aa178517c15384ceb7aa
Pete Maan <pete.maan+github@gmail.com>
2018-05-15 03:06:26 +0100

    ADD Invoke-PARClient.Tests

---
+++ Tests/Invoke-PARClient.Tests.ps1
@@ -0,0 +1,129 @@
+#Get Current Directory
+$Here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+#Get Function Name
+$FunctionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests.ps1"
+
+#Assume ModuleName from Repository Root folder
+$ModuleName = Split-Path (Split-Path $Here -Parent) -Leaf
+
+#Resolve Path to Module Directory
+$ModulePath = Resolve-Path "$Here\..\$ModuleName"
+
+#Define Path to Module Manifest
+$ManifestPath = Join-Path "$ModulePath" "$ModuleName.psd1"
+
+if( -not (Get-Module -Name $ModuleName -All)) {
+
+	Import-Module -Name "$ManifestPath" -Force -ErrorAction Stop
+
+}
+
+BeforeAll {
+
+	#$Script:RequestBody = $null
+
+}
+
+AfterAll {
+
+	#$Script:RequestBody = $null
+
+}
+
+Describe $FunctionName {
+
+	InModuleScope $ModuleName {
+
+		Context "Mandatory Parameters" {
+
+			$Parameters = @{Parameter = 'Server'},
+			@{Parameter = 'CommandParameters'},
+			@{Parameter = 'Password'},
+			@{Parameter = 'Credential'},
+			@{Parameter = 'PassFile'}
+
+			It "specifies parameter <Parameter> as mandatory" -TestCases $Parameters {
+
+				param($Parameter)
+
+				(Get-Command Invoke-PARClient).Parameters["$Parameter"].Attributes.Mandatory | Should Be $true
+
+			}
+
+		}
+
+		Context "Input" {
+
+			BeforeEach {
+
+				Mock Test-Path -MockWith {
+					$true
+				}
+
+				Mock Start-PARClientProcess -MockWith {
+					Write-Output @{}
+				}
+
+				$InputObj = [pscustomobject]@{
+					Server            = "SomeServer"
+					CommandParameters = "Some Command Parameters"
+					Password          = ConvertTo-SecureString "SomePassword" -AsPlainText -Force
+				}
+
+			}
+
+			It "executes command with password" {
+
+				$InputObj = [pscustomobject]@{
+					Server            = "SomeServer"
+					CommandParameters = "Some Command Parameters"
+					Password          = ConvertTo-SecureString "SomePassword" -AsPlainText -Force
+				}
+
+				$InputObj | Invoke-PARClient -verbose
+
+				Assert-MockCalled Start-PARClientProcess -Times 1 -Exactly -Scope It
+
+			}
+
+			It "executes command with credential" {
+
+				$InputObj = [pscustomobject]@{
+					Server            = "SomeServer"
+					CommandParameters = "Some Command Parameters"
+					Credential        = New-Object System.Management.Automation.PSCredential ("username", $(ConvertTo-SecureString "SomePassword" -AsPlainText -Force))
+				}
+
+				$InputObj | Invoke-PARClient -verbose
+
+				Assert-MockCalled Start-PARClientProcess -Times 1 -Exactly -Scope It
+
+			}
+
+			It "executes command with credential" {
+
+				$InputObj = [pscustomobject]@{
+					Server            = "SomeServer"
+					CommandParameters = "Some Command Parameters"
+					PassFile          = "SomeFile"
+				}
+
+				$InputObj | Invoke-PARClient -verbose
+
+				Assert-MockCalled Start-PARClientProcess -Times 1 -Exactly -Scope It
+
+			}
+
+
+		}
+
+		Context "Output" {
+
+
+
+		}
+
+	}
+
+}
