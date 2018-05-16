71b00003aee567d9436ce05741329b53bc9f84b6
Pete Maan <pete.maan+github@gmail.com>
2018-05-16 21:36:35 +0100

    UPDATE Invoke-PARClient.Tests
    Additional tests added

--- Tests/Invoke-PARClient.Tests.ps1
+++ Tests/Invoke-PARClient.Tests.ps1
@@ -51,6 +51,8 @@ Describe $FunctionName {
 
 			}
 
+
+
 		}
 
 		Context "Input" {
@@ -69,19 +71,46 @@ Describe $FunctionName {
 					Server            = "SomeServer"
 					CommandParameters = "Some Command Parameters"
 					Password          = ConvertTo-SecureString "SomePassword" -AsPlainText -Force
+					Port              = 1234
 				}
+
+
+			}
+
+			It "throws if `$PAR variable not set in script scope" {
+
+				{$InputObj | Invoke-PARClient} | Should Throw
+
+			}
+
+			It "throws if `$PAR variable does not have ClientPath property" {
+
+				$object = [PSCustomObject]@{
+					prop1 = "Value1"
+					prop2 = "Value2"
+				}
+				New-Variable -Name PAR -Value $object
+
+				{$InputObj | Invoke-PARClient} | Should Throw
+
+			}
+
+			it "does not throw after Set-PARConfiguration has set the `$PAR variable" {
+
+				Set-PARConfiguration -ClientPath "C:\SomePath\PARClient.exe"
+				{$InputObj | Invoke-PARClient} | Should Not throw
+
+			}
+
+			it "does not require Set-PARConfiguration to be run more than once" {
+
+				{$InputObj | Invoke-PARClient} | Should Not throw
 
 			}
 
 			It "executes command with password" {
 
-				$InputObj = [pscustomobject]@{
-					Server            = "SomeServer"
-					CommandParameters = "Some Command Parameters"
-					Password          = ConvertTo-SecureString "SomePassword" -AsPlainText -Force
-				}
-
-				$InputObj | Invoke-PARClient -verbose
+				$InputObj | Invoke-PARClient
 
 				Assert-MockCalled Start-PARClientProcess -Times 1 -Exactly -Scope It
 
@@ -95,7 +124,7 @@ Describe $FunctionName {
 					Credential        = New-Object System.Management.Automation.PSCredential ("username", $(ConvertTo-SecureString "SomePassword" -AsPlainText -Force))
 				}
 
-				$InputObj | Invoke-PARClient -verbose
+				$InputObj | Invoke-PARClient
 
 				Assert-MockCalled Start-PARClientProcess -Times 1 -Exactly -Scope It
 
@@ -109,12 +138,18 @@ Describe $FunctionName {
 					PassFile          = "SomeFile"
 				}
 
-				$InputObj | Invoke-PARClient -verbose
+				$InputObj | Invoke-PARClient
 
 				Assert-MockCalled Start-PARClientProcess -Times 1 -Exactly -Scope It
 
 			}
 
+			it "fails if PARClient cannot be found" {
+				Mock Test-Path -MockWith {
+					$false
+				}
+				{$InputObj | Invoke-PARClient} | Should throw
+			}
 
 		}
 
